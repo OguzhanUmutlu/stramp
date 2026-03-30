c8495892be5706cfe8a801b0009f9c13646ba0a7
OguzhanUmutlu <64753457+OguzhanUmutlu@users.noreply.github.com>
2026-03-30 10:58:30 +0300

    v2.4.0 - Fixes for ESM

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,23 +3,31 @@ import terser from "@rollup/plugin-terser";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import {defineConfig} from "rollup";
 
-export default [
-    defineConfig({
-        input: "dist/ts/Stramp.js",
-        output: [
-            {
-                file: "dist/index.js",
-                format: "esm"
-            },
-            {
-                file: "index.min.js",
-                format: "esm",
-                plugins: [terser()]
-            }
-        ],
-        plugins: [
-            nodeResolve({preferBuiltins: false}),
-            commonjs()
-        ]
-    })
-];
+export default defineConfig({
+    input: "dist/ts/Stramp.js",
+    output: [
+        {
+            file: "dist/index.js",
+            format: "esm",
+            sourcemap: true
+        },
+        {
+            file: "dist/index.cjs",
+            format: "cjs",
+            exports: "named",
+            sourcemap: true
+        },
+        {
+            file: "dist/index.min.js",
+            format: "esm",
+            plugins: [terser()],
+            sourcemap: true
+        }
+    ],
+    plugins: [
+        nodeResolve({
+            preferBuiltins: false
+        }),
+        commonjs()
+    ]
+});
